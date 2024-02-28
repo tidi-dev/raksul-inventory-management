338c26eb834a360fd92bb20f3eea5eb825af9041
tidi <tidi.dev1321@gmail.com>
2024-02-28 09:07:48 +0700

    Feat: Authentication

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,40 @@
+import { UserRepository } from '@/common/repositories';
+import {
+  ExistingPhoneNumberConstraint,
+  UniquePhoneNumberConstraint,
+} from '@/core/validators';
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtAuthStrategy } from './jwt/jwt-auth.strategy';
+import { UserLocalAuthStrategy } from './user/user-auth.strategy';
+
+@Module({
+  controllers: [AuthController],
+  imports: [
+    PassportModule,
+    JwtModule.registerAsync({
+      useFactory: async (configService: ConfigService) => {
+        return {
+          secret: configService.get<string>('JWT_SECRET'),
+          signOptions: {
+            expiresIn: '2h',
+          },
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [
+    AuthService,
+    UserLocalAuthStrategy,
+    JwtAuthStrategy,
+    UserRepository,
+    ExistingPhoneNumberConstraint,
+    UniquePhoneNumberConstraint,
+  ],
+})
+export class AuthModule {}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,37 @@
+import type { LoginDto, RegisterDto } from '@/common/dtos';
+import { decryptPhone } from '@/common/functions';
+import { UserRepository } from '@/common/repositories';
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private jwtService: JwtService,
+    private userRepository: UserRepository,
+  ) {}
+
+  async login({ phone_number }: LoginDto) {
+    const user =
+      await this.userRepository.updateTheLatestSignInTime(phone_number);
+    return {
+      access_token: this.jwtService.sign({ phone_number }),
+      user: {
+        ...user,
+        phone_number: decryptPhone(user.phone_number),
+      },
+    };
+  }
+
+  async register(registerDto: RegisterDto) {
+    const user = await this.userRepository.createUser(registerDto);
+
+    return {
+      access_token: this.jwtService.sign({ user }),
+      user: {
+        ...user,
+        phone_number: decryptPhone(user.phone_number),
+      },
+    };
+  }
+}
